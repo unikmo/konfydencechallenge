3a4ed85197cbdbcb1231597f24c911db6836b10b
unikmo <mbanwie@googlemail.com>
2026-08-27 03:12:26 +0200

    Allow licensed Pexels hero photography

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const CSP = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com",
   "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://upload.wikimedia.org https://commons.wikimedia.org",
+  "img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://upload.wikimedia.org https://commons.wikimedia.org https://images.pexels.com",
   "font-src 'self' data:",
   "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://*.myshopify.com",
   "frame-ancestors 'self'",
